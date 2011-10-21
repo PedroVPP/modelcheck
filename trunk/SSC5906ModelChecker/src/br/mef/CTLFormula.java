a0347ef449dd294aa2db2b93426dee8fd96df5d2
luizandre00@msn.com <luizandre00@msn.com@e9b5c5a6-a871-9bec-e655-be5495ab04cb>
2011-10-21 15:50:29 +0000

    CTLFormula atualizado

--- trunk/SSC5906ModelChecker/src/br/mef/CTLFormula.java
+++ trunk/SSC5906ModelChecker/src/br/mef/CTLFormula.java
@@ -1,13 +1,47 @@
 package br.mef;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CTLFormula {
 
 	private List<String> ctlExpression;
+	private List<State> states;
 
-	public CTLFormula(List<String> ctlExpression) {
-		this.ctlExpression = ctlExpression;
+	public CTLFormula(List<State> states) {
+		ctlExpression = new ArrayList<String>();
+		StringBuffer sb = new StringBuffer();
+		for (int i = 0; i < states.size(); i++) {
+			for (int j = 0; j < states.get(i).getValidProperties().size(); j++) {
+				if (states.get(i).getValidProperties().size() < j) {
+					sb.append(states.get(i).getValidProperties().get(i)
+							.getName()
+							+ ",");
+				} else {
+					sb.append(states.get(i).getValidProperties().get(i)
+							.getName());
+				}
+			}
+			ctlExpression.add(states.get(i).getName() + " " + sb.toString());
+		}
+	}
+
+	public CTLFormula() {
+		ctlExpression = new ArrayList<String>();
+		StringBuffer sb = new StringBuffer();
+		for (int i = 0; i < states.size(); i++) {
+			for (int j = 0; j < states.get(i).getValidProperties().size(); j++) {
+				if (states.get(i).getValidProperties().size() < j) {
+					sb.append(states.get(i).getValidProperties().get(i)
+							.getName()
+							+ ",");
+				} else {
+					sb.append(states.get(i).getValidProperties().get(i)
+							.getName());
+				}
+			}
+			ctlExpression.add(states.get(i).getName() + " " + sb.toString());
+		}
 	}
 
 	public List<String> getCTLExpression() {
@@ -16,6 +50,14 @@ public class CTLFormula {
 
 	public void setCTLExpression(List<String> ctlExpression) {
 		this.ctlExpression = ctlExpression;
+	}
+
+	public List<State> getStates() {
+		return states;
+	}
+
+	public void setStates(List<State> states) {
+		this.states = states;
 	}
 
 }
