2217657aff5fd7b120604e7349eb32475944497e
vania.neves@gmail.com <vania.neves@gmail.com@e9b5c5a6-a871-9bec-e655-be5495ab04cb>
2011-10-25 06:02:55 +0000

    Adicionada possibilidade de incluir expressões pela interface gráfica

--- trunk/SSC5906ModelChecker/src/br/mef/MEF.java
+++ trunk/SSC5906ModelChecker/src/br/mef/MEF.java
@@ -9,7 +9,9 @@ public class MEF {
 	
 	private static MEF instance;
 	
-	private ArrayList<State> states = new ArrayList<State>();	
+	private ArrayList<Property> properties = new ArrayList<Property>();
+	private ArrayList<State> states = new ArrayList<State>();
+	private ArrayList<Expression> expressions = new ArrayList<Expression>();
 	private State firsState;
 	
 	private Escrita novoDotArq;
@@ -21,6 +23,13 @@ public class MEF {
 	         instance = new MEF();
 	      return instance;
 	}
+   
+   public void cleanMEF(){
+	   this.states.clear();
+	   this.properties.clear();
+	   this.expressions.clear();
+	   this.firsState = null;
+   }
    
 	public void removeState(int index){
 		states.remove(index);
@@ -64,13 +73,21 @@ public class MEF {
 	}
 	
 	public void createMEF(){
-		this.novoDotArq = new Escrita("D:/teste.dot");
+		this.novoDotArq = new Escrita("mef.dot");
 		novoDotArq.abrir();
 		novoDotArq.escreverLinha("digraph mef {");
 		novoDotArq.novaLinha();		
     	for (int i = 0; i < states.size(); ++i) {
     		State state = (State) states.get(i);
-    		novoDotArq.escreverLinha(state.getName());
+			String linhaState = state.getName() + "[ label= \""+state.getName()+"\\n{";
+			ArrayList<Property> properties = state.getValidProperties();
+			for(int j=0; j<properties.size(); j++){
+				Property property = (Property) properties.get(j);
+				linhaState = linhaState + property.getName() + ", ";
+			}
+			
+			linhaState = linhaState.substring(0, linhaState.length() -2) + "}\"]";    		
+    		novoDotArq.escreverLinha(linhaState);
     		novoDotArq.novaLinha();
     	    ArrayList<State> filhos = state.getChildren();
 	    	for (int j = 0; j < filhos.size(); ++j) {
@@ -88,7 +105,14 @@ public class MEF {
 			novoDot.addln(novoDot.start_graph());
 			for(int i=0; i<states.size(); i++){
 				State state = (State) states.get(i);
-				novoDot.addln(state.getName());
+				String linhaState = state.getName() + "[ label= \""+state.getName()+"\\n{";
+				ArrayList<Property> properties = state.getValidProperties();
+				for(int j=0; j<properties.size(); j++){
+					Property property = (Property) properties.get(j);
+					linhaState = linhaState + property.getName() + ", ";
+				}
+				linhaState = linhaState.substring(0, linhaState.length() -2) + "}\"]";
+				novoDot.addln(linhaState);
 				ArrayList<State> filhos = state.getChildren();
 				for (int j = 0; j < filhos.size(); ++j) {
 		    		State filho = (State) filhos.get(j);
@@ -98,7 +122,7 @@ public class MEF {
 			novoDot.addln(novoDot.end_graph());
 			System.out.println(novoDot.getDotSource());
 			
-			this.out = new File("D:/testeout.jpg");		
+			this.out = new File("mef.jpg");		
 			novoDot.writeGraphToFile(novoDot.getGraph(novoDot.getDotSource()), out);
 		}
 		catch(Exception e){
@@ -111,9 +135,8 @@ public class MEF {
 	
 	
 	
-	//adicionando parte que genrecia as propriedades
-	private ArrayList<Property> properties = new ArrayList<Property>();
-		
+	//adicionando parte que gerencia as propriedades
+	
 	public void addProperty(Property n){
 		if(!properties.contains(n)){
 			properties.add(n);		  
@@ -133,5 +156,33 @@ public class MEF {
 	public ArrayList<Property> getProperties(){
 		return this.properties;
 	}
-		
+	
+	public void setState(ArrayList<State> states){
+		this.states = states;
+	}	
+
+	
+	//adicionando parte que gerencia as expressions
+	
+	public void addExpressions(Expression exp){
+		if(!expressions.contains(exp)){
+			expressions.add(exp);		  
+		}
+	}
+
+	public Expression getExpression(String name){
+
+		for (Expression exp : expressions) {
+			if(exp.getName().equals(name)){
+				return exp;
+			}
+		}
+		return null;
+	}
+	
+	public ArrayList<Expression> getExpressions(){
+		return this.expressions;
+	}
+	
+
 }
