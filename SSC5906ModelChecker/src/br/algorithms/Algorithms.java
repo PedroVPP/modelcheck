ff778007eac453dfa65c3c7b4422e7ae49b33c42
pedrovictor.pinheiro@gmail.com <pedrovictor.pinheiro@gmail.com@e9b5c5a6-a871-9bec-e655-be5495ab04cb>
2011-11-07 17:17:20 +0000

    - Deletado métodos desnecessários que serviam de Stub;

--- SSC5906ModelChecker/src/br/algorithms/Algorithms.java
+++ SSC5906ModelChecker/src/br/algorithms/Algorithms.java
@@ -12,39 +12,6 @@ public class Algorithms {
 
 	public static GraphvizFileMaker graphvizFileMaker; 
 	
-	public static boolean Alg_E(State st, Expression exp1, Expression exp2) {
-		boolean resposta = false;
-		// algoritmo para verificar se uma express�o e � v�lida
-		return resposta;
-	}
-
-	public static boolean Alg_Id(State st, Expression exp1) {
-		boolean resposta = false;
-		// algoritmo para verificar se uma propriedade � v�lida em um estado
-		// se o type dela for id, o name ser� uma propriedade...
-		ArrayList<Property> properties = st.getValidProperties();
-		for (int i = 0; i < properties.size(); ++i) {
-			Property prop = (Property) properties.get(i);
-			if (prop.getName().equals(exp1.getName())) {
-				resposta = true;
-				// st.addLabel(exp.getLabel());
-				// ficou faltando a parte de associar um label a uma express�o
-				// acho q daria certo se no icmcJCM.jj criasse um atributo
-				// global
-				// do tipo inteiro q fosse sendo incrementado sempre que uma
-				// nova
-				// express�o � criada e setando essa vari�vel no label
-				// (expression.setLAbel());
-				// ai, nesse trecho adicionaria o label da express�o
-				// depois, para verificar se a express�o � v�lida em um
-				// determinado
-				// estado, bastaria verificar se o estado possui o label
-				break;
-			}
-		}
-		return resposta;
-	}
-
 	/**
 	 * @author Pedro Pinheiro
 	 * @param state
