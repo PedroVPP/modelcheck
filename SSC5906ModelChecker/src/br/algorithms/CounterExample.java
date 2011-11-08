e66c58d66eb088a90ba9b01722c3a2dffd09d320
vania.neves@gmail.com <vania.neves@gmail.com@e9b5c5a6-a871-9bec-e655-be5495ab04cb>
2011-11-08 23:29:03 +0000

    Criação da classe de contra exemplo

---
+++ SSC5906ModelChecker/src/br/algorithms/CounterExample.java
@@ -0,0 +1,44 @@
+package br.algorithms;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import br.mef.Expression;
+import br.mef.State;
+
+public class CounterExample {
+	private State state;
+	private Expression exp;
+	private HashMap<State, State> states = new HashMap<State, State>();
+	private ArrayList<State> validos = new ArrayList<State>();
+	
+	public CounterExample(State state, Expression expression) {
+		this.state = state;
+		this.exp = expression;
+	}
+	public State getState() {
+		return state;
+	}
+	public void setState(State state) {
+		this.state = state;
+	}
+	public Expression getExp() {
+		return exp;
+	}
+	public void setExp(Expression exp) {
+		this.exp = exp;
+	}
+	
+	public ArrayList<State> getValidos() {
+		return this.validos;
+	}
+
+	public void addStateValido(State state) {
+		this.validos.add(state);
+	}	
+	
+	public HashMap<State, State> getTransicoes(){
+		return this.states;
+	}
+
+}
